0ef59e9cb1219b38d3b02a274f227f54bac81b32
opilane <opilane@HP-CZC2349HT2>
2025-04-09 17:35:36 +0300

    Add project files.

---
+++ FunktsioonideClass.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TARgv24_C_
+{
+    internal class FunktsioonideClass
+    {
+        public static float Calculator(int num1, int num2)
+        {
+            float calculator = 0;
+            calculator = num1 * num2;
+            return calculator;
+        }
+        public static string switchKasuta(int a)
+        {
+            string text = "";
+            switch (a)
+            {
+                case 1: text = "E"; break;
+                case 2: text = "T"; break;
+                case 3: text = "K"; break;
+                case 4: text = "N"; break;
+                case 5: text = "R"; break;
+                case 6: text = "L"; break;
+                case 7: text = "P"; break;
+
+                default:
+                    {
+                        text = "Tundmatu";
+                        break;
+                    }
+            }
+            return text;
+        }
+        public static string neighbor(string hum1, string hum2)
+        {
+            string text = "Сегодня " + hum1 + " и " + hum2 + " будут соседями по скамейке";
+            return text;
+        }
+        public static void temp(int t)
+        {
+            try
+            {
+                if (t < 18)
+                {
+                    Console.WriteLine("В комнате слишком холодно, всего лишь" + t + " градусов");
+                }
+                else
+                {
+                    Console.WriteLine("В комнате приемлемая температура - " + t + " градусов");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        public static float value(float v)
+        {
+            float value = v / 70 * 100;
+            return value;
+        }
+        public static void height(int h)
+        {
+            try
+            {
+                if (h <= 160)
+                {
+                    Console.WriteLine("Вы низкий");
+                }
+                else if (h < 180)
+                {
+                    Console.WriteLine("Вы среднего роста");
+                }
+                else if (h >= 180)
+                {
+                    Console.WriteLine("Вы высокий");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        public static double floor(double len, double wid, double price)
+        {
+            double area = len * wid;
+            return area * price;
+        }
+        public static double shop(bool m, bool b, bool w)
+        {
+            double mp = 80;
+            double bp = 50;
+            double wp = 120;
+            double total = 0;
+            if (m) total += mp;
+            if (b) total += bp;
+            if (w) total += wp;
+            return total;
+        } 
+    }
+}
---
+++ MainClass.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C_
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.OutputEncoding = Encoding.UTF8;
+            /*
+            int a = 0;
+            string text = "Python";
+            char taht= 'A';
+            double num = 5.4536237287;
+            float num1 = 2;
+             Console.Write("What is your name? ");
+            text = Console.ReadLine();
+            Console.WriteLine("Hello!\n" + text);
+            Console.WriteLine("Hello!\n{0}", text);
+            if (text.ToLower()=="juku")
+            {
+                Console.WriteLine("Lahme kinno!");
+                try
+                {
+                    Console.WriteLine("{0}\n Kui vana sa oled?", text);
+                    int vanus=int.Parse(Console.ReadLine());
+                    if (vanus<=0 || vanus>100)
+                    {
+                        Console.WriteLine("Viga!");
+                    }
+                    else if (vanus>0 && vanus <=6)
+                    {
+                        Console.WriteLine("Tasuta");
+                    }
+                    else if (vanus<= 15)
+                    {
+                        Console.WriteLine("Lastepilet");
+                    }
+                    else if (vanus <= 65)
+                    {
+                        Console.WriteLine("Lastepilet");
+                    }
+                    else if (vanus <= 100)
+                    {
+                        Console.WriteLine("Lastepilet");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Olen hõivatud!");
+            }
+            Console.WriteLine("Number 2: ");
+            int num2=int.Parse(Console.ReadLine());
+            // Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", num1, num2, num1 *  num2);
+            num1=FunktsioonideClass.Calculator(a, num2);
+            Console.ReadKey(); */
+
+            // Task Random
+            Random rnd = new Random();
+            int a = rnd.Next(1, 7);
+            Console.WriteLine(a);
+            string rndfunc = FunktsioonideClass.switchKasuta(a);
+            Console.WriteLine(rndfunc);
+
+            // Task 1 - Соседи по скамейке
+            Console.WriteLine("Как тебя зовут?");
+            string hum1 = Console.ReadLine();
+            Console.WriteLine("Назови второе имя");
+            string hum2 = Console.ReadLine();
+            string neighbor = FunktsioonideClass.neighbor(hum1, hum2);
+            Console.WriteLine(neighbor);
+
+            // Task 2 - Заменить пол
+            Console.WriteLine("Длина комнаты?");
+            double len=double.Parse(Console.ReadLine());
+            Console.WriteLine("Ширина комнаты?");
+            double wid = double.Parse(Console.ReadLine());
+            Console.WriteLine("Цена за кв.метр?");
+            double price = double.Parse(Console.ReadLine());
+            double total = FunktsioonideClass.floor(len, wid, price);
+            Console.WriteLine("Стоимость замены пола: "+total);
+
+            // Task 3 - Изначальная цена
+            Console.WriteLine("Введите цену товара, со скидкой в 30%");
+            float v = float.Parse(Console.ReadLine());
+            float value = FunktsioonideClass.value(v);
+            Console.WriteLine(value);
+
+            // Task 4 - Температура в комнате
+            Console.WriteLine("Какая температура в комнате?");
+            int t = int.Parse(Console.ReadLine());
+            FunktsioonideClass.temp(t);
+
+            // Task 5 - Рост человека
+            Console.WriteLine("Какого вы роста?");
+            int h = int.Parse(Console.ReadLine());
+            FunktsioonideClass.height(h);
+
+            // Task 7 - Магазин
+            Console.WriteLine("Хотите ли вы купить молоко? (Да/Нет)");
+            bool m = Console.ReadLine().ToLower() == "Да";
+            Console.WriteLine("Хотите ли вы купить хлеб? (Да/Нет)");
+            bool b = Console.ReadLine().ToLower() == "Да";
+            Console.WriteLine("Хотите ли вы купить выпечку? (Да/Нет)");
+            bool w = Console.ReadLine().ToLower() == "Да";
+            double totals = FunktsioonideClass.shop(m, b, w);
+            Console.WriteLine("Стоимость всех продуктов: " + totals);
+        }
+    }
+}
