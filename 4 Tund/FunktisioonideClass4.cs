10a077b8c89563c96d30e3e1be3c053427d4a337
Matviei Orlov <theskonel@gmail.com>
2025-05-06 20:25:48 +0300

    4 Tund

---
+++ 4 Tund/FunktisioonideClass4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARgv24_C_._4.Tund
+{
+    internal class FunktsioonideClass_4osa
+    {
+        public static void Main(string[] args)
+        {
+            List<Toode> tooded = new List<Toode>();
+        }
+
+    }
+}
